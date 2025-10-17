615e50bdbdcdc4d08bee078aacac6281025bf948
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-10-17 14:52:49 +0300

    struct of the objects

--- models/objectsDatatypes.go
+++ models/objectsDatatypes.go
@@ -5,5 +5,34 @@ type VideoRequest struct {
 }
 
 type VideoResponse struct {
-	
+	URL       string `json:"url"`
+	Source    string `json:"source"`
+	ID        string `json:"id"`
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Thumbnail string `json:"thumbnail"`
+	Medias    []struct {
+		URL     string `json:"url"`
+		Quality string `json:"quality"`
+		Width   int    `json:"width"`
+		Height  int    `json:"height"`
+		Ext     string `json:"ext"`
+	} `json:"medias"`
+	Error bool `json:"error"`
+}
+
+type YTDLPOutput struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Uploader  string `json:"uploader"`
+	Thumbnail string `json:"thumbnail"`
+	Formats   []struct {
+		URL    string `json:"url"`
+		Ext    string `json:"ext"`
+		Format string `json:"format"`
+		Width  int    `json:"width"`
+		Height int    `json:"height"`
+		Acodec string `json:"acodec"`
+		Vcodec string `json:"vcodec"`
+	} `json:"formats"`
 }
